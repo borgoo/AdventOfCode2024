79e201b0dd797d06e5a3355b6350576f5e8218a5
borgoo <borgo-ale@libero.it>
2025-02-04 16:03:25 +0100

    - Added impossible/loop exceptions - ok Day17 SolB

---
+++ AdventOfCode2024/Exceptions/ImpossibleException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Exceptions
+{
+    internal class ImpossibleException(string? message = "Not solvable.") : Exception(message)
+    {
+
+    }
+}
---
+++ AdventOfCode2024/Exceptions/LoopException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Exceptions
+{
+    internal class LoopException(string? message = "Infinite loop.") : Exception(message)
+    {
+
+    }
+
+
+}
