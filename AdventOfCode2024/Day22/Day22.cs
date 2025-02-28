5fd449dd53c6052f138fe9807d594723cd3edbab
borgoo <borgo-ale@libero.it>
2025-02-28 11:27:48 +0100

    feat(Day22): Implemented SolB for Day22.

--- AdventOfCode2024/Day22/Day22.cs
+++ AdventOfCode2024/Day22/Day22.cs
@@ -4,12 +4,13 @@ namespace AdventOfCode2024.Day22
     internal class Day22 : Day
     {
 
-        private const int SOLA_NUM_OF_ITERATIONS = 2000;
+        private const int NUM_OF_ITERATIONS = 2000;
 
         private const int MUL_64 = 6; // 2^6 = 64 -> *64 is equivalent to shifting left by 6 psx
-        private const int PRUNE_FACTOR_MOD_16777216 = 16777215; // 2^24 = 16777216 -> Modulo 2^24 is equivalent to AND with (2^24 - 1)
+        private const int PRUNE_FACTOR_MOD_16777216 = 16777215; // 2^24 = 16777216 -> Mod 2^24 is equivalent to AND with (2^24 - 1)
         private const int DIV_32 = 5; // 2^5 = 32 -> /32 is equivalent to shifting right by 5 (PSX)
         private const int MUL_2048 = 11; // 2^11 = 2048 -> *2048 is equivalent to shifting left by 11 psx
+        private const int LAST_DIGIT_MOD = 10; //getting last digit is equivalent to Mod 10
         protected override object SolveA(string input)
         {
             int[] startingSecretNumbers = [.. input.Split("\r\n").Select(e => Convert.ToInt32(e))];
@@ -18,7 +19,7 @@ namespace AdventOfCode2024.Day22
             foreach (int start in startingSecretNumbers) {
 
                 long inc = start;
-                for (int i = 0; i < SOLA_NUM_OF_ITERATIONS; i++) {
+                for (int i = 0; i < NUM_OF_ITERATIONS; i++) {
                     inc = CalcNextSecretNumber(inc);
                 }
                 ans += inc;
@@ -29,7 +30,38 @@ namespace AdventOfCode2024.Day22
 
         protected override object SolveB(string input)
         {
-            throw new NotImplementedException();
+            MaxCalculator maxCalculator = new();
+            int[] startingSecretNumbers = [.. input.Split("\r\n").Select(e => Convert.ToInt32(e))];
+
+            for (int j = 0; j < startingSecretNumbers.Length; j++) {
+
+                int start = startingSecretNumbers[j];
+                int[] currentBuyerOfferPrices = new int[NUM_OF_ITERATIONS + 1];
+                int[] diffHistory = new int[NUM_OF_ITERATIONS + 1];
+                currentBuyerOfferPrices[0] = GetBuyerOfferPrices(start);
+                diffHistory[0] = currentBuyerOfferPrices[0];
+                long inc = start;
+                HashSet<(int, int, int, int)> seen = [];
+                for (int i = 0; i < NUM_OF_ITERATIONS; i++)
+                {
+                    inc = CalcNextSecretNumber(inc);
+                    currentBuyerOfferPrices[i + 1] = GetBuyerOfferPrices(inc);
+                    if (i > 0) diffHistory[i] = currentBuyerOfferPrices[i] - currentBuyerOfferPrices[i - 1];
+                    if (i > 3) {
+
+                        (int, int, int, int) key = (diffHistory[i-3], diffHistory[i - 2], diffHistory[i - 1], diffHistory[i]);
+                        if (seen.Contains(key)) continue;
+                        seen.Add(key);
+                        int val = currentBuyerOfferPrices[i];
+                        maxCalculator.Add(key, val);
+                    }
+                }
+            }
+
+            return maxCalculator.GetMaxSum();
+
+
+
         }
 
         private static long CalcNextSecretNumber(long currSecretNumber) {
@@ -50,7 +82,41 @@ namespace AdventOfCode2024.Day22
 
         }
 
-        
+        private static int GetBuyerOfferPrices(long currSecretNumber) {
 
+            return Convert.ToInt32(currSecretNumber % LAST_DIGIT_MOD);
+        }
+
+
+
+        private class MaxCalculator {
+
+            private (int, int, int, int)? _currMaxKey;
+
+            private readonly Dictionary<(int, int, int, int), int> _keySums = [];
+
+            public void Add((int, int, int, int) key, int val)
+            {
+
+                if (!_keySums.ContainsKey(key))
+                {
+                    _keySums.Add(key, 0);
+                }
+                _keySums[key] += val;
+
+                if (!_currMaxKey.HasValue || _keySums[key] > GetMaxSum()) _currMaxKey = key;
+
+            }
+
+            public int GetMaxSum() {
+
+                if (!_currMaxKey.HasValue) throw new NullReferenceException();
+
+                return _keySums[_currMaxKey.Value];
+            }
+        } 
     }
+
+
 }
+
