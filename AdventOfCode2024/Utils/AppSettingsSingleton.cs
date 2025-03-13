579f8e296b65a84f87d25bc992f7ec199b953a4a
borgoo <borgo-ale@libero.it>
2025-03-13 20:00:41 +0100

    feat(AppSettingsSingleton): Added AppSettings singleton.

---
+++ AdventOfCode2024/Utils/AppSettingsSingleton.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Utils
+{
+    internal sealed class AppSettingsSingleton
+    {
+        private static readonly Lazy<AppSettingsSingleton> _instance = new Lazy<AppSettingsSingleton>(() => new AppSettingsSingleton());
+        public static AppSettingsSingleton Instance => _instance.Value;
+
+        public IConfiguration Configuration { get; private set; }
+
+        private AppSettingsSingleton() {
+            Configuration = null!;
+        }
+
+        public void Initialize(IConfiguration config)
+        {
+            Configuration = config ?? throw new ArgumentNullException(nameof(config));
+        }
+    }
+}
